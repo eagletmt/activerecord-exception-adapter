c673197234e7b2e14e5610ad99bc6764657b161c
Kohei Suzuki <eagletmt@gmail.com>
2014-08-13 15:54:57 +0900

    Adapter is automatically loaded by activerecord

--- lib/activerecord-exception-adapter.rb
+++ lib/activerecord-exception-adapter.rb
@@ -7,6 +7,5 @@ module ActiveRecordExceptionAdapter
 end
 
 ActiveSupport.on_load(:active_record) do
-  require 'active_record/connection_adapters/exception_adapter'
   ActiveRecord::Base.extend ActiveRecordExceptionAdapter
 end
